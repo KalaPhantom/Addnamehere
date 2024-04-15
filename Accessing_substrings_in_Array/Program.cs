3252da44096a03406f2e433778a8fa851030bb7a
KalaPhantom <152006110+KalaPhantom@users.noreply.github.com>
2024-04-15 21:16:09 +0800

    Add files via upload

---
+++ Accessing_substrings_in_Array/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] listOfStats = {"Power: 10 atk dmg", "STR: 10u" ,"DEX: 90u", "Type: Projectile"};
+
+        foreach (string a in listOfStats){
+            int posindex = a.IndexOf(":");
+            Console.WriteLine(a.Substring(posindex));
+        }
+    }
+}
